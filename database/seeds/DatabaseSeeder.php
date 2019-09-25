b9a60a911683f583d461854585a4a3e0b1c8dca9
marvin-baga-ey <marvin.baga@sg.ey.com>
2019-09-25 19:01:23 +0800

    Add database seed for user and title

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use App\Title;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +13,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Title::create([
+            'title' => 'Division Head',
+            'slug' => 'division-head'
+        ]);
+        
+        Title::create([
+            'title' => 'Project Consultant',
+            'slug' => 'project-consultant'
+        ]);
+        
+        Title::create([
+            'title' => 'UX Researcher',
+            'slug' => 'ux-researcher'
+        ]);
+        
+        Title::create([
+            'title' => 'Data Analyst',
+            'slug' => 'data-analyst'
+        ]);
+        
+        Title::create([
+            'title' => 'Designer',
+            'slug' => 'designer'
+        ]);
+        
+        Title::create([
+            'title' => 'Front-end Developer',
+            'slug' => 'front-end-developer'
+        ]);
+        
+        Title::create([
+            'title' => 'Back-end Developer',
+            'slug' => 'back-end-developer'
+        ]);
+        
+        User::create([
+            'first_name' => 'Marvin',
+            'last_name' => 'Baga',
+            'slug' => 'marvin-baga',
+            'email' => 'marvin.baga@sg.ey.com',
+            'password' => Hash::make('1234aA'),
+            'title_id' => 6,
+            'active' => 1
+        ]);
     }
 }
